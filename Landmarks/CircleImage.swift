8234ef28aacac69ef5afc74a324d1d51ba98b88c
prince <prince18pat@gmail.com>
2019-10-27 23:31:40 +0900

    add circle image view

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Prince Kumar on 2019/10/27.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
