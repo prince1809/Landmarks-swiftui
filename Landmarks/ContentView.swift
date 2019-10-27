84d5339b84832edf74ce5aa44766af11af91612d
prince <prince18pat@gmail.com>
2019-10-27 22:33:40 +0900

    swiftui project created

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Prince Kumar on 2019/10/27.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Prince")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
